5a508261223bbc035b9dcdb84b7b4ecbda2c97d3
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-04-17 09:58:40 +0700

    WPU 11 N+1 Problem

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Reference
+
+[Belajar Laravel 8 ](https://www.youtube.com/playlist?list=PLFIM0718LjIWiihbBIq-SWPU6b6x21Q_2)
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
@@ -35,13 +35,13 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
-        'posts' => $category->posts,
+        'posts' => $category->posts->load('category', 'author'),
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
-        'posts' => $author->posts,
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });

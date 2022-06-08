676dc7469a20b03f3956b71edd76e01dc38d7284
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-06-08 14:43:42 +0700

    query scope section done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,10 +19,16 @@ class Post extends Model
         //     return $query->where('title', 'like', '%' . request('search') . '%')
         //           ->orWhere('body', 'like', '%' . request('search') . '%');
         // }
-        if(isset($filters['search']) ? $filters['search'] : false) {
-            return $query->where('title', 'like', '%' . $filters['search'] . '%')
-                  ->orWhere('body', 'like', '%' . $filters['search'] . '%');
-        }
+
+        // if(isset($filters['search']) ? $filters['search'] : false) {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')
+        //           ->orWhere('body', 'like', '%' . $filters['search'] . '%');
+        // }
+
+        $query->when($filters['search'] ?? false, function($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')
+                  ->orWhere('body', 'like', '%' . $search . '%');
+        });
     }
 
     public function category()
